84c1498b6b6dc5e7cb654845275f54d4046d4f75
Sparrow <omarashrafgeddawy@gmail.com>
2024-07-31 21:24:16 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SRT2LRC",
-    version="0.1",
+    name="SubLyric",
+    version="1.0",
     packages=find_packages(),
     install_requires=[],
     author="Oni-Sparrow",
@@ -12,7 +12,7 @@ setup(
     description="Converts from SRT to LRC and vice versa.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/your_library",
+    url="https://github.com/Oni-Sparrow/SubLyric",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
